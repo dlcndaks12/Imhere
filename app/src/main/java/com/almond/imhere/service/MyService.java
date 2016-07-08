26adfb8d7f9bce2e47e48e4d41e719cc7eabbca6
lcm <dlcndaks12@naver.com>
2016-07-08 17:31:04 +0900

    Initial commit

---
+++ app/src/main/java/com/almond/imhere/service/MyService.java
@@ -0,0 +1,20 @@
+package com.almond.imhere.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
